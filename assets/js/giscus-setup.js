bb8ef0b8f31d32ef44f2b7f9b4bfa7658955d5ec
slcluckyboy <slc19980722@hanyang.ac.kr>
2025-12-26 21:08:58 +0000

    Deploying to gh-pages from @ slcluckyboy/linchunsun.github.io@9198a2075774aeed9e29eecf13908addde870d33 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "slcluckyboy/slcluckyboy.github.io",
+    "data-repo": "slcluckyboy/linchunsun.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
